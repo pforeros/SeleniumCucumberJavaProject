301d096feae0f1fb8d85f07dc2abfe4967ee2a6d
Pilar Forero <pilarandrea@gmail.com>
2024-03-11 12:01:38 -0500

    First Build

---
+++ src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,26 @@
+package steps;
+
+import io.cucumber.java.en.*;
+import pages.GooglePage;
+
+public class GoogleSteps {
+
+        GooglePage google = new GooglePage();
+
+        @Given("^I am on the Google search page$")
+        public void navigateToGoogle() {
+                google.navigateToGoogle();
+        }
+
+        @When("^I enter a search criteria$")
+        public void enterSearchCriteria() {
+        }
+
+        @And("^click on the search button$")
+        public void clickSearchButton() {
+        }
+
+        @Then("^the results match the criteria$")
+        public void validateResults() {
+        }
+}
