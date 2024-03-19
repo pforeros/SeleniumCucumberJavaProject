73892ab4b2108e4377fa9b58e5fc33d6ff07a91e
Pilar Forero <pilarandrea@gmail.com>
2024-03-19 15:18:18 -0500

    Clase 83

--- src/test/java/pages/GooglePage.java
+++ src/test/java/pages/GooglePage.java
@@ -1,11 +1,20 @@
 package pages;
 
+
 public class GooglePage extends BasePage {
+
+  private String searchButton = "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']";
+
+
     public GooglePage(){
         super(driver);
       }
 
 public void navigateToGoogle(){
     navigateTo("https://www.google.com");
+  }
+
+  public void clickGoogleSearch(){
+    clickElement(searchButton);
   }
 }
